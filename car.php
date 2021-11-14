159c281e20467392aa29ac7ff0de6bc5965ce51b
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-11-14 12:07:52 +0100

    POO4

--- car.php
+++ car.php
@@ -2,21 +2,11 @@
 
 require_once("vehicle.php");
 
-class Car extends Vehicle implements LightableInterface
+class Car extends Vehicle
 
 {
     public function __construct(string $color, int $nbSeats, string $energy)
     {
         parent::__construct($color, $nbSeats, $energy);
-    }
-
-    public function switchOn(): bool
-    {
-        return true;
-    }
-
-    public function switchOff(): bool
-    {
-        return false;
     }
 }
