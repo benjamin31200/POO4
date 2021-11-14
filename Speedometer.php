94cee21a9d7a960872efe58cf95489bbb1a3b413
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-11-14 14:06:43 +0100

    POOStatic

---
+++ Speedometer.php
@@ -0,0 +1,19 @@
+<?php
+
+class Speedometer
+{
+   
+        public const conversionKM = 1.609344;
+        public const conversionMILE = 0.6215;
+
+        public static function convertKmToMiles($km) {
+            return $miles = $km * self::conversionMILE; 
+        }
+
+        public static function convertMilestoKm($miles) {
+            return $km = $miles / self::conversionKM; 
+        }
+
+};
+
+echo Speedometer::convertKmToMiles(10);
